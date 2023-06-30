aa9a88bca719161208f2c63990d588c4ac14ae87
Ali Nurdin <alinurdin159@gmail.com>
2023-06-30 21:52:55 +0700

    fix feature favorite

--- lib/ui/favorite_page.dart
+++ lib/ui/favorite_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:submission_2_restaurant_app/providers/favorite_provider.dart';
+import 'package:submission_2_restaurant_app/ui/restaurant_detail_page.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({super.key});
@@ -16,12 +17,25 @@ class FavoritePage extends StatelessWidget {
               var item = favoriteProvider.favorites[index];
 
               return ListTile(
-                title: Text(item.name),
-                subtitle: Text(item.rating.toString()),
+                title: Text(
+                  item.name,
+                  style: const TextStyle(
+                    fontSize: 20
+                  ),
+                ),
+                subtitle: Text(
+                  'Rating: ${item.rating.toString()}',
+                  style: const TextStyle(
+                    color: Colors.grey
+                  ),
+                ),
                 trailing: IconButton(
-                  icon: const Icon(Icons.visibility),
+                  icon: const Icon(
+                    Icons.visibility,
+                    color: Colors.white,
+                  ),
                   onPressed: () {
-                    
+                    Navigator.pushNamed(context, RestaurantDetailPage.routeName, arguments: item.id);
                   },
                 ),
               );
